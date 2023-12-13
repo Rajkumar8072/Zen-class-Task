b1108314bad21e05ee3f710101989ee522fd07a7
Rajkumar8072 <126305993+Rajkumar8072@users.noreply.github.com>
2023-12-13 14:01:18 +0530

    Add files via upload

---
+++ Day-5/jsonresume.js
@@ -0,0 +1,25 @@
+const resume = {
+  name: "Rajkumar",
+  title: "MERN Stack Developer",
+  github: "https://github.com/Rajkumar8072",
+  email: "er.rajsurya@gmail.com",
+  phone: "+91-8675819507",
+  education: [
+    {
+      degree: "BE in Mechanical Engineering",
+      university: "UCEA",
+      start_date: "2015",
+      end_date: "2019",
+      percentage: "72",
+    },
+  ],
+  skills: [
+    "HTML-5",
+    "CSS-3",
+    "React JS",
+    "Bootstrap-5",
+    "Git",
+    "GitHub",
+    "Netlify",
+  ],
+};
